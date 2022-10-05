93d1ebc8b7e2aafac6b1816ef831c0b387ed81f0
dianazmitrevich <zmitrevichdiana@gmail.com>
2022-10-05 18:44:30 +0300

    Fix footer

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,7 +1,2 @@
-   <div class="container">
-      <footer class="py-3 my-2">
-         <p class="text-center text-muted">2022</p>
-      </footer>
-   </div>
    <script src="/resources/js/script.js"></script>
 </body>
